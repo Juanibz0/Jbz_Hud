34fe186d31349298903de90741bd94f27082fd91
Juan_ibz05 <73550686+Juanibz05@users.noreply.github.com>
2021-05-09 19:22:51 +0200

    Add files via upload

--- locales/es.lua
+++ locales/es.lua
@@ -1,1 +1,72 @@
+-- Spanish
+Locales['es'] = {
+	['you_are_on_location'] = 'Usted está en <strong>%s</strong>',
 
+	['date_format'] = {
+		default = 'Hoy es <strong>%s %s, %s</strong>',
+		simple = 'Hoy es <strong>%s, %s</strong>',
+		simpleWithHours = '<strong>%s</strong>, <strong>%s, %s</strong>',
+		withWeekday = 'Hoy es <strong>%s, %s %s, %s</strong>',
+		withHours = '<strong>%s</strong>, <strong>%s.%s.%s</strong>',
+		withWeekdayAndHours = '<strong>%s</strong>, <strong>%s, %s %s, %s</strong>'
+	},
+
+	['weekDay_0'] = 'domingo',
+	['weekDay_1'] = 'lunes',
+	['weekDay_2'] = 'martes',
+	['weekDay_3'] = 'miércoles',
+	['weekDay_4'] = 'jueves',
+	['weekDay_5'] = 'viernes',
+	['weekDay_6'] = 'sábado',
+
+	['day_1'] = '1.',
+	['day_2'] = '2.',
+	['day_3'] = '3.',
+	['day_4'] = '4.',
+	['day_5'] = '5.',
+	['day_6'] = '6.',
+	['day_7'] = '7.',
+	['day_8'] = '8.',
+	['day_9'] = '9.',
+
+	['day_10'] = '10.',
+	['day_11'] = '11.',
+	['day_12'] = '12.',
+	['day_13'] = '13.',
+	['day_14'] = '14.',
+	['day_15'] = '15.',
+	['day_16'] = '16.',
+	['day_17'] = '17.',
+	['day_18'] = '18.',
+	['day_19'] = '19.',
+
+	['day_20'] = '20.',
+	['day_21'] = '21.',
+	['day_22'] = '22.',
+	['day_23'] = '23.',
+	['day_24'] = '24.',
+	['day_25'] = '25.',
+	['day_26'] = '26.',
+	['day_27'] = '27.',
+	['day_28'] = '28.',
+	['day_29'] = '29.',
+
+	['day_30'] = '30.',
+	['day_31'] = '31.',
+
+	['month_0'] = 'Enero',
+	['month_1'] = 'Febrero',
+	['month_2'] = 'Marzo',
+	['month_3'] = 'Abril',
+	['month_4'] = 'Mayo',
+	['month_5'] = 'Junio',
+	['month_6'] = 'Julio',
+	['month_7'] = 'Agosto',
+	['month_8'] = 'Septiembre',
+	['month_9'] = 'Noviembre',
+	['month_10'] = 'Diciembre',
+	['month_11'] = 'Agosto',
+
+	['toggleui'] = 'Ver/Quitar el HUD',
+
+}
